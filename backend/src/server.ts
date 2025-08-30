c274a53e2169353e7368de2ad42c6416b438ff00
Prachi2142001 <prachibirla21@gmail.com>
2025-08-30 12:45:49 +0530

    Updated server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,36 +3,33 @@ dotenv.config();
 
 import express from "express";
 import cors from "cors";
-import productsRoutes from "./routes/productRoutes";  
+import productsRoutes from "./routes/productRoutes";
 import storefrontProducts from "./routes/getProducts";
-import cartRoutes from "./routes/cartRoutes";   
+import cartRoutes from "./routes/cartRoutes";
 import cookieParser from "cookie-parser";
-
 
 const app = express();
 
-app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000", credentials: true })); 
-app.use(express.json());
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://commercetools-fullstack.vercel.app",
+];
 
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+    exposedHeaders: ["x-cart-id"],
+  })
+);
+app.use(express.json());
 
 app.get("/health", (_req, res) => res.json({ status: "ok" }));
 
-
 app.use("/api/products", productsRoutes);
 app.use("/api/storefront/products", storefrontProducts);
-
-
-app.use("/api/storefront", cartRoutes); 
-
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-    exposedHeaders: ["x-cart-id"], 
-  })
-);
-
+app.use("/api/storefront", cartRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
